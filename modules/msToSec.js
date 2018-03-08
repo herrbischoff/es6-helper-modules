827a276faffce26c318ad70db7c7390f5af88331
Marcel Bischoff <marcel@herrbischoff.com>
2018-03-08 16:42:40 +0100

    Initial commit
    Add msToSec function
    
    Create modules folder to keep things tidy
    
    Add more descriptive comment

---
+++ modules/msToSec.js
@@ -0,0 +1,15 @@
+/**
+ * Milliseconds to fractions of seconds
+ *
+ * This can be helpful for CSS animation timings you want to count up
+ * dynamically. Just change the `animation-delay` property.
+ *
+ * @param {number} ms milliseconds
+ * @returns {number} seconds
+ */
+let msToSec = (ms) => {
+  let s = ((ms % 60000) / 1000).toFixed(2);
+  return s;
+};
+
+export default msToSec;
