348d5f0ff08b6b2868cde1c4e739d098295618db
Marcel Bischoff <marcel@herrbischoff.com>
2018-04-26 20:38:18 +0200

    Create project files and make modules importable

---
+++ index.js
@@ -0,0 +1,6 @@
+import formatBytes from './modules/formatBytes.js';
+import msToSec from './modules/msToSec.js';
+import removeArrayItemByValue from './modules/removeArrayItemByValue.js';
+import shuffleArray from './modules/shuffleArray.js';
+
+export {formatBytes, msToSec, removeArrayItemByValue, shuffleArray};
